79f18ab40e7ef248063e6db93f62406b4afae286
Vikash KUmar Yadav <yadav621gzp@gmail.com>
2023-10-07 01:35:03 +0530

    addd3

--- src/containers/navbar/App.jsx
+++ src/containers/navbar/App.jsx
@@ -1,5 +1,5 @@
 import {useEffect, useRef, useState} from 'react';
-import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import {HashLink as Link} from 'react-router-hash-link';
 import styled from 'styled-components';
 import HomePage from '../Home/HomePage';
@@ -137,14 +137,14 @@ const NAVBAR = ({}) => {
         />
       </nav>
 
-      <Switch>
+      <Routes>
         <Route path="/blog" exact={true}>
           <MdxContent />
         </Route>
         <Route path="/" exact={true}>
           <HomePage />
         </Route>
-      </Switch>
+      </Routes>
     </Router>
   );
 };
